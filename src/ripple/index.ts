fc01add27da68ce81b6dfbf4b21275d361e41b44
cano <mahorowa3088@gmail.com>
2019-09-30 08:43:01 +0900

    🚧 リスナ登録

--- src/ripple/index.ts
+++ src/ripple/index.ts
@@ -1,6 +1,20 @@
+interface RippleEventHandler {
+  pointerDown: EventListener;
+  pointerUp: EventListener;
+  pointerOut: EventListener;
+  animationEnd: EventListener;
+}
+
 class Ripple {
   private node: HTMLElement;
   private ripple: HTMLDivElement;
+
+  private eventHandler: RippleEventHandler = {
+    pointerDown: (e: PointerEvent) => this.rippleStart(e),
+    pointerUp: () => this.pointerUp(),
+    pointerOut: () => this.pointerOut(),
+    animationEnd: (e: AnimationEvent) => this.animationEnd(e)
+  };
 
   constructor(node: HTMLElement) {
     this.node = node;
@@ -9,7 +23,22 @@ class Ripple {
     this.ripple.classList.add('pmd-ripple-x');
 
     this.node.appendChild(this.ripple);
+
+    this.node.addEventListener('pointerdown', this.eventHandler.pointerDown, { passive: true });
+    this.node.addEventListener('pointerup', this.eventHandler.pointerUp, { passive: true });
+    this.node.addEventListener('pointerout', this.eventHandler.pointerOut, { passive: true });
+    this.node.addEventListener('animationend', this.eventHandler.animationEnd, { passive: true });
   }
+
+  rippleStart(e: PointerEvent): void {}
+
+  rippleEnd(): void {}
+
+  pointerUp(): void {}
+
+  pointerOut(): void {}
+
+  animationEnd(e: AnimationEvent): void {}
 }
 
 export { Ripple };
