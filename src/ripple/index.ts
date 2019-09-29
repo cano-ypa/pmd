89dd4e5106ce72d3c5ff10d496158197d43562a8
cano <mahorowa3088@gmail.com>
2019-09-30 08:37:35 +0900

    🚧 初期化処理

--- src/ripple/index.ts
+++ src/ripple/index.ts
@@ -1,5 +1,15 @@
 class Ripple {
-  constructor() {}
+  private node: HTMLElement;
+  private ripple: HTMLDivElement;
+
+  constructor(node: HTMLElement) {
+    this.node = node;
+
+    this.ripple = document.createElement('div');
+    this.ripple.classList.add('pmd-ripple-x');
+
+    this.node.appendChild(this.ripple);
+  }
 }
 
 export { Ripple };
