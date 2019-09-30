22ea37c4673dd2214d48e07355f7bf86c8c1a0bc
cano <mahorowa3088@gmail.com>
2019-09-30 11:27:42 +0900

    ⚙️ config

--- scripts/webpack/scssBundleFactory.ts
+++ scripts/webpack/scssBundleFactory.ts
@@ -1,12 +1,46 @@
 import BundleFactory from './bundleFactory';
 import * as webpack from 'webpack';
+import path from 'path';
 
-class ScssBundleFactory extends BundleFactory {
-  private createScssCommonConfig({ output, chunks }: { output: string; chunks: string | string[] }): webpack.Configuration {}
+class TsBundleFactory extends BundleFactory {
+  private createScssCommonConfig({ output, chunks }: { output: string; chunks: string | { [s: string]: string } }): webpack.Configuration {
+    return {
+      mode: 'production',
 
-  public createCombinedConfig(): webpack.Configuration {}
+      output: { filename: '[name]', path: output },
 
-  public createALaCarteConfig(): webpack.Configuration {}
+      entry: chunks,
+
+      module: {
+        rules: [
+          {
+            test: /\.scss$/,
+            use: ['scss-loader']
+          }
+        ]
+      },
+
+      resolve: { extensions: ['.scss'] },
+
+      devtool: 'source-map'
+    };
+  }
+
+  public createCombinedConfig(): webpack.Configuration {
+    const output = path.resolve(__dirname, './build/Combined');
+    const chunks = path.resolve(__dirname, './src/pedeal-material/index.scss');
+
+    return this.createScssCommonConfig({ output, chunks });
+  }
+
+  public createALaCarteConfig(): webpack.Configuration {
+    const output = path.resolve(__dirname, './build/ALaCarte');
+    const chunks = {
+      ripple: path.resolve(__dirname, './src/ripple/index.scss')
+    };
+
+    return this.createScssCommonConfig({ output, chunks });
+  }
 }
 
-export default ScssBundleFactory;
+export default TsBundleFactory;
--- scripts/webpack/tsBundleFactory.ts
+++ scripts/webpack/tsBundleFactory.ts
@@ -1,12 +1,47 @@
 import BundleFactory from './bundleFactory';
 import * as webpack from 'webpack';
+import path from 'path';
 
 class TsBundleFactory extends BundleFactory {
-  private createTsCommonConfig({ output, chunks }: { output: string; chunks: string | { [s: string]: string } }): webpack.Configuration {}
+  private createTsCommonConfig({ output, chunks }: { output: string; chunks: string | { [s: string]: string } }): webpack.Configuration {
+    return {
+      mode: 'production',
 
-  public createCombinedConfig(): webpack.Configuration {}
+      output: { filename: '[name]', path: output },
 
-  public createALaCarteConfig(): webpack.Configuration {}
+      entry: chunks,
+
+      module: {
+        rules: [
+          {
+            test: /\.ts$/,
+            exclude: /node_modules/,
+            use: ['ts-loader']
+          }
+        ]
+      },
+
+      resolve: { extensions: ['.ts'] },
+
+      devtool: 'source-map'
+    };
+  }
+
+  public createCombinedConfig(): webpack.Configuration {
+    const output = path.resolve(__dirname, './build/Combined');
+    const chunks = path.resolve(__dirname, './src/pedeal-material/index.ts');
+
+    return this.createTsCommonConfig({ output, chunks });
+  }
+
+  public createALaCarteConfig(): webpack.Configuration {
+    const output = path.resolve(__dirname, './build/ALaCarte');
+    const chunks = {
+      ripple: path.resolve(__dirname, './src/ripple/index.ts')
+    };
+
+    return this.createTsCommonConfig({ output, chunks });
+  }
 }
 
 export default TsBundleFactory;
