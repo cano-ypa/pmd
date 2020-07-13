96f1714af0f29a1ba75973904b0a076872622646
Cano <contact.sasasaky@gmail.com>
2020-07-13 13:44:55 +0900

    webpack 関連ファイルを削除

--- scripts/webpack/bundleFactory.ts
+++
@@ -1,9 +0,0 @@
-import * as webpack from 'webpack';
-
-interface BundleFactory {
-  createCombined(): webpack.Configuration;
-
-  createALaCarte(): webpack.Configuration;
-}
-
-export default BundleFactory;
--- scripts/webpack/type.ts
+++
@@ -1,9 +0,0 @@
-import * as webpack from 'webpack';
-
-type WebpackChunks = string | string[] | webpack.Entry | webpack.EntryFunc;
-
-type WebpackRules = webpack.RuleSetRule[];
-
-type ConfigOpt = { output: string; chunks: WebpackChunks };
-
-export { WebpackChunks, WebpackRules, ConfigOpt };
